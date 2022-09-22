91d5f63924a1d27e42ad2e94c3a99ffec17bba4a
Jordan Brown <j.d.brown329@gmail.com>
2022-09-22 14:25:23 -0500

    first commit

---
+++ Classesx01/Program.cs
@@ -0,0 +1,8 @@
+﻿using Classesx01;
+
+Car myCar = new Car();
+myCar.Make = "Hyundai";
+myCar.Model = "Sonnata";
+myCar.Year = 2003;
+
+Console.WriteLine($"I have a {myCar.Make} {myCar.Model} which was made in the year {myCar.Year}");
